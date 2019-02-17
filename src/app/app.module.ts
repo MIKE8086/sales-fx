96436bb564f9ed34b3418994e78933df0d458452
Gogs <gogs@fake.local>
2019-02-17 14:16:26 -0600

    Nuevo componente ng-cmp-test01
    Comando utilizado: ng generate component components/ng-cmp-test01
    --entryComponent=true --export=true --flat=true --lintFix=true
    --selector=cmp-test01 --skipTests=true
    
    Error del comando anterior:
    src/app/components/ng-cmp-test01.component.ts[4, 13]:
    The selector of the component "NgCmpTest01Component"
    should have prefix "app"
    (https://angular.io/styleguide#style-02-07)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgCmpTest01Component } from './components/ng-cmp-test01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgCmpTest01Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [NgCmpTest01Component],
+  entryComponents: [NgCmpTest01Component]
 })
 export class AppModule { }
---
+++ src/app/components/ng-cmp-test01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmp-test01',
+  templateUrl: './ng-cmp-test01.component.html',
+  styleUrls: ['./ng-cmp-test01.component.scss']
+})
+export class NgCmpTest01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
