e1a125a113700865d11d485489f3a9c8ab474a47
Gogs <gogs@fake.local>
2019-02-17 15:12:35 -0600

    Se agregan módulos con ruteo (child)
    Comando utilizado:
    ng generate module modules/ng-module-tst01
    --flat=true --lintFix=true --routing=true --routingScope=Child

---
+++ src/app/modules/ng-module-tst01.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgModuleTst01RoutingModule } from './ng-module-tst01-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgModuleTst01RoutingModule
+  ]
+})
+export class NgModuleTst01Module { }
