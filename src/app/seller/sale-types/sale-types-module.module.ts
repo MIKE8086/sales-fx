c9b81a64a664c822b04bd239f16104cb713c0de3
Gogs <gogs@fake.local>
2019-02-17 15:31:01 -0600

    Se agregan módulos hijo (seller) con ruteo
    Comando utilizado:
    ng generate module seller/sale-types/sale-types-module
    --flat=true --lintFix=true --routing=true --routingScope=Child

---
+++ src/app/seller/sale-types/sale-types-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SaleTypesModuleRoutingModule } from './sale-types-module-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SaleTypesModuleRoutingModule
+  ]
+})
+export class SaleTypesModuleModule { }
