8452b64ed7448f4239ae62d7de1bbc7b580b792b
Gogs <gogs@fake.local>
2019-02-17 15:40:51 -0600

    Se agrega componente sale-types-table en el modulo seller
    Comando utilizado:
    ng generate component
    seller/sale-types/sale-types-table/sale-types-table
    --entryComponent=true --export=true --flat=true
    --lintFix=true --selector=sale-types-table --skipTests=true
    --module=seller/sale-types/sale-types-module.module.ts
    
    Error and warnings:
    Warning: The 'no-use-before-declare' rule requires type information.
    Warning: The 'deprecation' rule requires type information.
    ERROR:
    src/app/seller/sale-types/sale-types-table/sale-types-table.component.ts[4, 13]
    :The selector of the component "SaleTypesTableComponent"
    should have prefix "app" (https://angular.io/styleguide#style-02-07)

---
+++ src/app/seller/sale-types/sale-types-table/sale-types-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sale-types-table',
+  templateUrl: './sale-types-table.component.html',
+  styleUrls: ['./sale-types-table.component.scss']
+})
+export class SaleTypesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
